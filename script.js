d4bbf6c85c5f4acca2c819fd51183dffbe1d749d
Felicia Wright <fedub1@yahoo.com>
2020-08-10 22:19:25 -0400

    Added initial files

---
+++ script.js
@@ -0,0 +1,77 @@
+var button = document.getElementById("answer.buttons")
+var question = document.getElementById("question")
+start-btn.addEventListener("click", startQuiz)
+next-btn.addEventListener("click", nextQuestion)
+console.log("click", nextQuestion)
+
+function startGame() {
+    console.log("StartNow")
+    set1stQuestion()
+}
+
+for (var i = 0; i < questionEl.length; i++) {
+    var question = Question[i];
+
+    var li = document.createElement("li");
+    li.textContent = todo;
+    todoList.appendChild(li);
+  }
+}
+
+// When form is submitted...
+todoForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+    //function 2ndQuestion() {
+
+
+    //}0
+
+
+    //function selectedAnswer (){
+
+
+    //}
+
+    //var questions = [
+       // {
+           // question: "Commonly used data types do not include:",
+            //answers: [
+               // { text: 'strings', correct: true },
+               // text: 'booleans', wrong: false},
+       // text: 'alerts', wrong: false},
+//text: 'numbers', wrong: false},
+
+//}
+
+//]
+//timer starts
+
+//<button onclick="myFunction()">Try it</button>
+
+//}
+//function myFunction() {
+  //alert("present 1st question");
+//}
+
+//present 1st question
+
+//present 2nd question
+
+//present 3rd question
+
+//present 4th question
+
+//incorrect answer deducts time from clock
+
+//all questions answered or timer reached 0
+
+//game over
+
+//save initials and score
+
+//}):
+
+
+
+
+
